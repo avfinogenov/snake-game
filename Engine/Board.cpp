4131c9be11d390c494c61e70269ff8e58502d8ce
not_sure <avfinogenov@gmail.com>
2020-05-08 17:29:37 +0300

    1

--- Engine/Board.cpp
+++ Engine/Board.cpp
@@ -4,36 +4,53 @@
 
 
 
-Board::Board(Graphics & gfx)
+Board::Board(Graphics& gfx)
 	: gfx(gfx),
 	snek()
 {
-	for (int x = 0;x < cellNumX;x++)//sdsdsd
+	for (int x = 0; x < cellNumX; x++)//sdsdsd
 	{
-		for (int y = 0;y < cellNumY;y++)
+		for (int y = 0; y < cellNumY; y++)
 		{
 			cell[x][y] = Colors::Black;
 			guest[x][y] = false;
 		}
 	}
 	SetSnek();
-	for (int x = 0;x < cellNumX;x++)
+	for (int x = 0; x < cellNumX; x++)
 	{
 		int y = 0;
 		guest[x][y] = true;
-		y = cellNumY-1;
+		y = cellNumY - 1;
 		guest[x][y] = true;
 	}
-	for (int y = 0;y < cellNumY;y++)
+	for (int y = 0; y < cellNumY; y++)
 	{
 		int x = 0;
 		guest[x][y] = true;
-		x = cellNumX-1;
+		x = cellNumX - 1;
 		guest[x][y] = true;
+	}
+	int count = 50;
+	std::random_device rd;
+	std::mt19937 rng(rd());
+	std::uniform_int_distribution<int> sDist(0, cellNumX * cellNumY);
+
+	while (count > 0)
+	{
+		int randomnumber = sDist(rng);
+		int Xrand, Yrand;
+		Yrand = randomnumber / cellNumX;
+		Xrand = randomnumber - Yrand * cellNumX;
+		if (cell[Xrand][Yrand] == Colors::Black)
+		{
+			SetPoison(Xrand, Yrand);
+			count--;
+		}
+
 	}
 
 
-	
 	SetGoal();
 }
 
@@ -44,50 +61,50 @@ void Board::WCell(Location in_loc, Color c)
 
 void Board::Draw(Graphics& gfx)
 {
-	
-	for (int x=0;x<cellNumX;x++)
+
+	for (int x = 0; x < cellNumX; x++)
 	{
-		for (int y=0;y<cellNumY;y++)
-		{			
-			gfx.DrawRect(x*width, y*height, width, height, cell[x][y]);
+		for (int y = 0; y < cellNumY; y++)
+		{
+			gfx.DrawRect(x * width, y * height, width, height, cell[x][y]);
 
 		}
 	}
-	
+
 
 }
 
 void Board::MakeStep(float dt)
 {
 
-	if (isEaten)				
+	if (isEaten)
 	{
 		snek.Segment(snek.GetSeg(snek.tail - 1), snek.tail);//создаем новый хвост
 	}
-	
-		snek.MakeStep(dt);
-		if(snek.stepIsDone){
-			CheckVln();
-			int tmp = snek.tail;
-			Location tmpLc = snek.GetSeg(tmp - 1);
-			WCell(tmpLc, emptyColor);
+
+	snek.MakeStep(dt);
+	if (snek.stepIsDone) {
+		CheckVln();
+		int tmp = snek.tail;
+		Location tmpLc = snek.GetSeg(tmp - 1);
+		WCell(tmpLc, emptyColor);
 
 
-		for (int n = snek.tail - 1;n >= 1;n--)//с хвоста к голове
+		for (int n = snek.tail - 1; n >= 1; n--)//с хвоста к голове
 		{
 			snek.Segment(snek.GetSeg(n - 1), n);//записать н-1 в н
-			
+
 		}
 		WCell(snek.GetSeg(1), bodyColor);
-				//snek.IncStep(dt);
+		//snek.IncStep(dt);
 
-		
+
 		snek.ApplyStep();
 		guest[(int)tmpLc.x][(int)tmpLc.y] = false;
 		if (!vln) {
 			Location head = snek.GetSeg(0);
 			WCell(head, headColor);//записать нарисовать голову
-		
+
 			guest[(int)head.x][(int)head.y] = true;
 		}
 		if (isEaten)				//если голодная
@@ -96,25 +113,61 @@ void Board::MakeStep(float dt)
 			isEaten = false;//перевариваем фрукт
 		}
 		snek.stepIsDone = false;
-		}
+	}
 
 }
 
 void Board::CheckVln()
 {
 	Location l1 = snek.GetSeg(0);
-	Location l2= snek.RStep();
+	Location l2 = snek.RStep();
 	Location in_loc;
 	if (l2.x != l2.y) {
 		in_loc.x = l1.x + l2.x;
 		in_loc.y = l1.y + l2.y;
-	if(guest[in_loc.x][in_loc.y])/// (guest[l1.x][l1.y])
-	{
 
-		vln = true;
+
+
+		if (cell[in_loc.x][in_loc.y] == Colors::Green)
+		{
+
+		}
+		if (cell[in_loc.x][in_loc.y] == Colors::Gray)
+		{
+			vln = true;
+		}
+		if (cell[in_loc.x][in_loc.y] == Colors::Magenta)
+		{
+			snek.vs++;
+		}
+		if (cell[in_loc.x][in_loc.y] == Colors::LightGray)
+		{
+			vln = true;
+		}
+		///if(guest[in_loc.x][in_loc.y])/// (guest[l1.x][l1.y])
+		///{
+		///
+		///	vln = true;
+		///}
+	}
+
+}
+
+void Board::CreateObstacle(int i, int j)
+{
+	// в свободную клетку засунуть преграду
+	if (cell[i][j] == Color(Colors::Black))
+	{
+		cell[i][j] = ObstacleColor;
 	}
 }
 
+void Board::SetPoison(int i, int j)
+{
+	if (cell[i][j] == Color(Colors::Black))
+	{
+		cell[i][j] = PoisonColor;
+	}
 }
 
 void Board::PaintGoal(Goal& g)
@@ -122,57 +175,73 @@ void Board::PaintGoal(Goal& g)
 	cell[(int)g.loc.x][(int)g.loc.y] = GoalColor;
 }
 
-void Board::Update(MainWindow & wnd)
+void Board::Update(MainWindow& wnd)
 {
 	snek.Update(wnd);
 }
 
 void Board::SetGoal()
 {
-	int q = (cellNumX * cellNumY);
-	int p = cellNumX * 2 + cellNumY * 2; 
-	snek.tail;
-	freeCell = q - p - snek.tail;
+	int count = 1;
 	std::random_device rd;
 	std::mt19937 rng(rd());
-	std::uniform_int_distribution<int> sDist(0, freeCell);
-	int x = 0;
-	int y = 0;
-	int randomnumber = sDist(rng);
-	for (int i = 0;i< cellNumX*cellNumY;i++)
+	std::uniform_int_distribution<int> sDist(0, cellNumX * cellNumY);
+
+	while (count > 0)
 	{
-		x = i - (i / cellNumX)*cellNumX;
-		y = i / cellNumX;
-		if (!guest[x][y])
+		int randomnumber = sDist(rng);
+		int Xrand, Yrand;
+		Yrand = randomnumber / cellNumX;
+		Xrand = randomnumber - Yrand * cellNumX;
+		if (cell[Xrand][Yrand] == Colors::Black)
 		{
-			randomnumber--;
+			g.loc.x = Xrand;
+			g.loc.y = Yrand;
+			WCell(g.loc, GoalColor);
+			count--;
 		}
-		if (randomnumber==0)
-		{
-			break;
-		}
+
 	}
-	g.loc.x = x;
-	g.loc.y = y;
-	WCell(g.loc, GoalColor);
-	
+	//g.loc.x = x;
+	//g.loc.y = y;
+	//WCell(g.loc, GoalColor);
+
 }
 
 void Board::ConsumeCheck()
 {
 	Location ls = snek.GetSeg(0);
 	Location lg = g.loc;
-	if (((int)lg.x == (int)ls.x) && ((int)lg.y ==(int) ls.y))//сравниваем положения головы и фрукта
+	if (((int)lg.x == (int)ls.x) && ((int)lg.y == (int)ls.y))//сравниваем положения головы и фрукта
 	{
 		SetGoal();
 		// отправляем фрукт в новые кординаты
 
 		isEaten = true;//змея проглатывает фрукт
-		PaintGoal(g);
-							/*if (dif>1)
-							{
-							dif--;
-							}*/
+		int count = 1;
+		std::random_device rd;
+		std::mt19937 rng(rd());
+		std::uniform_int_distribution<int> sDist(0, cellNumX * cellNumY);
+
+		while (count > 0)
+		{
+			int randomnumber = sDist(rng);
+			int Xrand, Yrand;
+			Yrand = randomnumber / cellNumX;
+			Xrand = randomnumber - Yrand * cellNumX;
+			if (cell[Xrand][Yrand] == Colors::Black)
+			{
+				CreateObstacle(Xrand, Yrand);
+				count--;
+			}
+
+		}
+
+		//PaintGoal(g);
+			 /*if (dif>1)
+			 {
+			 dif--;
+			 }*/
 	}
 
 }
@@ -181,12 +250,12 @@ void Board::ConsumeCheck()
 
 void Board::SetSnek()
 {
-	for (int i = snek.tail - 1; i > 0;i--) {
+	for (int i = snek.tail - 1; i > 0; i--) {
 		WCell(snek.GetSeg(i), bodyColor);
-		int x= snek.GetSeg(i).x;
-		int y= snek.GetSeg(i).y;
+		int x = snek.GetSeg(i).x;
+		int y = snek.GetSeg(i).y;
 		guest[x][y] = true;
-		}
+	}
 	WCell(snek.GetSeg(0), headColor);
 	int x = snek.GetSeg(0).x;
 	int y = snek.GetSeg(0).y;
